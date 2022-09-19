8a253b6b1e24d7d11722f83b4b4d875a89694d57
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-09-19 19:13:09 +0700

    Express static file

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,8 +1,7 @@
 import express from "express";
 
 const configViewEngine = (app) => {
-    app.use(express.static('./src/public'))
-
+    app.use(express.static('public'))
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
 }
