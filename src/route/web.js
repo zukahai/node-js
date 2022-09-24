9f034827041e7be09966f562311c11cd47b83583
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-09-24 22:42:58 +0700

    Fix file

--- src/route/web.js
+++ src/route/web.js
@@ -4,28 +4,22 @@ import HomeController from '../controller/HomeController';
 import multer from 'multer';
 import path from 'path';
 let appRoot = require('app-root-path');
-let router = express.Router();
 import { Router } from 'express';
 import RouteGroup from 'express-route-grouping';
 
 const root = new RouteGroup('/', Router());
 
-const fooMiddleware = (req, res, next) => {
-    console.log('foo');
-    next();
-}
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function(req, file, cb) {
         cb(null, appRoot + "/public/images/uploads/");
     },
 
-    filename: function (req, file, cb) {
+    filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
 
-const imageFilter = function (req, file, cb) {
+const imageFilter = function(req, file, cb) {
     // Accept images only
     if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
         req.fileValidationError = 'Only image files are allowed!';
@@ -35,11 +29,6 @@ const imageFilter = function (req, file, cb) {
 };
 
 let upload = multer({ storage: storage, fileFilter: imageFilter });
-
-const barMiddleware = (req, res, next) => {
-    console.log('bar');
-    next();
-}
 
 const homeRouter = (app) => {
 
